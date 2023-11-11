088d6113f8176815d67bfcc6609d0c55c773ec39
sakiso <aki4s.dev@gmail.com>
2023-11-11 22:39:19 +0900

    ローディング画面とりあえず

---
+++ lib/features/loading_overlay/loading_overlay.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+//
+// 全面表示のローディング
+//
+class LoadingOverlay extends StatelessWidget {
+  const LoadingOverlay({super.key, required this.visible});
+
+  //表示状態
+  final bool visible;
+
+  @override
+  Widget build(BuildContext context) {
+    return visible
+        ? Container(
+            decoration: BoxDecoration(
+              color:
+                  Theme.of(context).colorScheme.inverseSurface.withAlpha(140),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  width: 80,
+                  height: 80,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 8,
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                        Theme.of(context).colorScheme.inversePrimary),
+                  ),
+                ),
+              ],
+            ),
+          )
+        : Container();
+  }
+}
--- lib/features/top/top_page.dart
+++ lib/features/top/top_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../providers/wish_item_provider.dart';
 import '../../services/wish_item_builder_from_image_file.dart';
+import '../loading_overlay/loading_overlay.dart';
 
 class TopPage extends ConsumerWidget {
   const TopPage({Key? key}) : super(key: key);
@@ -13,22 +14,31 @@ class TopPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: Center(
-          child: Column(
+      body: Stack(
+        fit: StackFit.expand,
         children: [
-          // 画像追加ボタン
-          CircleAvatar(
-            backgroundColor: Theme.of(context).colorScheme.primary,
-            child: IconButton(
-              onPressed: () async {
-                await textRecognizeFromPickedFile(context, ref);
-              },
-              icon: const Icon(Icons.add_photo_alternate_rounded),
-              color: Theme.of(context).colorScheme.onPrimary,
+          Center(
+            child: Column(
+              children: [
+                // 画像追加ボタン
+                CircleAvatar(
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  child: IconButton(
+                    onPressed: () async {
+                      await textRecognizeFromPickedFile(context, ref);
+                    },
+                    icon: const Icon(Icons.add_photo_alternate_rounded),
+                    color: Theme.of(context).colorScheme.onPrimary,
+                  ),
+                ),
+              ],
             ),
           ),
+          const LoadingOverlay(
+            visible: true,
+          ),
         ],
-      )),
+      ),
       appBar: AppBar(
         title: Text(
           "top",
@@ -47,6 +57,7 @@ class TopPage extends ConsumerWidget {
     FilePickerResult? result = await FilePicker.platform.pickFiles();
 
     //todo: 読み込み中にローディングを出してダブルサブミット防止
+
     if (result != null) {
       final targetFile = File(result.files.single.path!);
       // todo: 画像じゃなかったらリターン
